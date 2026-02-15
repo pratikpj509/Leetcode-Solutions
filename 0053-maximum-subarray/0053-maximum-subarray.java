d7763df80fe1c6a4f7b0ac034a9960fce6166e51
pratikpj509 <118659140+pratikpj509@users.noreply.github.com>
2026-02-15 15:36:26 +0530

    Time: 1 ms (99.64%), Space: 77.6 MB (18.52%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int currSum = nums[0];   // running sum
+        int maxSum = nums[0];    // best seen so far
+
+        for (int i = 1; i < nums.length; i++) {
+            // decide whether to start a new subarray or continue old one
+            currSum = Math.max(nums[i], currSum + nums[i]);
+            
+            // update global max
+            maxSum = Math.max(maxSum, currSum);
+        }
+
+        return maxSum;
+    }
+}
+
+
+// Let’s walk through like a flashlight traveling across hills:
+
+// i	nums[i]	currSum calculation	currSum	maxSum
+// 0	-2	start	                    -2	-2
+// 1	1	max(1, -2+1=-1) → 1	1	1
+// 2	-3	max(-3, 1-3=-2) → -2	-2	1
+// 3	4	max(4, -2+4=2) → 4	4	4
+// 4	-1	max(-1, 4-1=3) → 3	3	4
+// 5	2	max(2, 3+2=5) → 5	5	5
+// 6	1	max(1, 5+1=6) → 6	6	6
+// 7	-5	max(-5, 6-5=1) → 1	1	6
+// 8	4	max(4, 1+4=5) → 5	5	6
